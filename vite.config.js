ec01ecf6689e5faa131e6a078abf19149444965f
Tarak517 <jampanitarak62@gmail.com>
2025-11-25 21:21:53 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/portfolio-site/',
+  base: "/my-portfolio/",
+});
 
-=======
->>>>>>> 46dc7598b1f5130ced97dfa777a83a823b454167
-})
+
